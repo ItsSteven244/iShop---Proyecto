03b0f1be16b74b498a45ecf2972c1d4fe09b1129
Steven <steven.alexis21@gmail.com>
2026-06-11 22:17:27 -0500

    docs: agregar comentarios mas explicativos en main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,21 +4,21 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	chimw "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5"                  //Importa el router chi
+	chimw "github.com/go-chi/chi/v5/middleware" //antes de una peticion pasa por filtros
 
 	"github.com/ItsSteven244/iShop---Proyecto/internal/handlers"
 	"github.com/ItsSteven244/iShop---Proyecto/internal/storage"
 )
 
 func main() {
-	// 1. Crear el storage del módulo correctivo.
+	// 1. Crear el storage del módulo correctivo / tanto para ordenes, procesos y evidencias.
 	correctivoStorage := storage.NuevaMemoriaCorrectivo()
 
 	// 2. Configurar el router principal.
-	r := chi.NewRouter()
-	r.Use(chimw.Logger)
-	r.Use(chimw.Recoverer)
+	r := chi.NewRouter()   //Se crea el router vacio, listo para registros
+	r.Use(chimw.Logger)    //como un registro, imprime mensaje/logs
+	r.Use(chimw.Recoverer) //si huo un problema no cae el server
 
 	// 3. Montar el subrouter del módulo correctivo.
 	r.Mount("/api/v1/correctivos", handlers.CorrectivosRouter(correctivoStorage))
